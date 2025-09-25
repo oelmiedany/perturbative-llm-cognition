7c78cfe3486ce903926395eb2fce7bfe1c87d2bf
omarelmiedany <omarelmiedany@outlook.com>
2025-09-25 12:21:34 +0000

    caching added to conversation loop for efficiency

--- experiments/chat.py
+++ experiments/chat.py
@@ -1,4 +1,6 @@
 from perturbative_llm_cognition import core
 
-print('hello world')
-core.conversation_loop()
+model_id = 'unsloth/mistral-7b-instruct-v0.3'
+tokenizer, model = core.load_tokenizer_and_model(model_id)
+
+core.conversation_loop(tokenizer, model)
